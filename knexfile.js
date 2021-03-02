8ee61a0fa2e9c7c55bc3590e1293e8fc6ddf9c6e
Kuljeet <kbhengura@gmail.com>
2021-03-02 16:28:26 +0530

    adding knexfile

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+// Update with your config settings.
+
+module.exports = {
+  "client": "mysql",
+  "connection": {
+    "host" : "localhost",
+    "user" : "root",
+    "password" : "1234",
+    "database" : "Indicator"
+  }
+};
