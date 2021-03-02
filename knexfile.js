966e99137c3fbb403fb9d0d4e0fac1a48cca314a
Kuljeet <kbhengura@gmail.com>
2021-03-02 20:20:24 +0530

    host = *

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,8 @@ module.exports = {
   client: 'pg',
   version: '7.2',
   connection: {
+    host : ['*'],
+    port : 5432,
     user : 'postgres',
     password : 'admin1234',
     database : 'sensei_koa'
