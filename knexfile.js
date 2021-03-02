684532f48c736686d8f1bdab86605e904a1d0691
Kuljeet <kbhengura@gmail.com>
2021-03-02 20:28:03 +0530

    added db_url

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,11 @@ module.exports = {
   client: 'pg',
   version: '7.2',
   connection: {
-    host : ['*'],
+    host : 'localhost'
     port : 5432,
     user : 'postgres',
     password : 'admin1234',
-    database : 'sensei_koa'
+    database : 'sensei_koa',
+    "use_env_variable": "DATABASE_URL"
   }
 }
