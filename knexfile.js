49b362aacc017fc58cc09a63a626a48865793485
Kuljeet <kbhengura@gmail.com>
2021-03-02 20:15:02 +0530

    remove host+port

--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,6 @@ module.exports = {
   client: 'pg',
   version: '7.2',
   connection: {
-    host : '127.0.0.1',
-    port : 5432,
     user : 'postgres',
     password : 'admin1234',
     database : 'sensei_koa'
