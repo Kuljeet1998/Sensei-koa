f67222fbb148c5b6589a4ab39e7843e975b09d88
Kuljeet <kbhengura@gmail.com>
2021-03-02 20:58:52 +0530

    database url specified/changed

--- knexfile.js
+++ knexfile.js
@@ -4,11 +4,11 @@ module.exports = {
   client: 'pg',
   version: '7.2',
   connection: {
-    host: "localhost",
+/*    host: "localhost",
     port : 5432,
     user : 'postgres',
     password : 'admin1234',
     database : 'sensei_koa',
-    use_env_variable: "postgres://fbcvnfcjsunupi:0406bf83512cf45cc2430a1ff1a2d60194139cab081d98ffbd1deac4488c349f@ec2-3-232-163-23.compute-1.amazonaws.com:5432/d463teparurj33"
+*/    use_env_variable: "postgres://fbcvnfcjsunupi:0406bf83512cf45cc2430a1ff1a2d60194139cab081d98ffbd1deac4488c349f@ec2-3-232-163-23.compute-1.amazonaws.com:5432/d463teparurj33"
   }
 }
