85fb86fb62b7d7cd3cec4ff05e775cf890e5c209
Kuljeet <kbhengura@gmail.com>
2021-03-02 16:49:39 +0530

    added port to config

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,7 @@ module.exports = {
   "client": "mysql",
   "connection": {
     "host" : "localhost",
+    "port" : 3306,
     "user" : "root",
     "password" : "1234",
     "database" : "Indicator"
