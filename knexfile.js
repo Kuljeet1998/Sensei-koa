b54ad2ced2ec0b4825eeaf7efb9119e32550c11f
Kuljeet <kbhengura@gmail.com>
2021-03-03 12:53:58 +0530

    config changes

--- knexfile.js
+++ knexfile.js
@@ -13,18 +13,67 @@
   }
 }*/
 
-var config = process.env.DATABASE_URL
+/*var config = process.env.DATABASE_URL
 if(config!==undefined)
 {   
     console.log(config)
-    /*config += '&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory'*/
-    config += ssl:true
-}
+    config += '&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory'
+}*/
+
+/*const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 module.exports ={
     client: 'pg',
-    connection: config || { user: 'postgres', 
+    connection: process.env.DATABASE_URL || { user: 'postgres', 
                                                 database: 'sensei_koa',
                                                 host: "localhost",
                                                 password : 'admin1234',
                                                 port : 5432}
+}
+
+const Pool = require('pg-pool');
+const url = require('url')
+
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
+*/
+const config = {
+  user: "fbcvnfcjsunupi",
+  password: "0406bf83512cf45cc2430a1ff1a2d60194139cab081d98ffbd1deac4488c349f",
+  host: 'ec2-3-232-163-23.compute-1.amazonaws.com',
+  port: '5432',
+  database: "d463teparurj33",
+  ssl: true,
+  sslfactory:"org.postgresql.ssl.NonValidatingFactory"
+};
+
+/*const pool = new Pool(config);*/
+
+
+
+var config1 = process.env.DATABASE_URL
+if(config1!==undefined)
+{   
+    module.exports = {
+    client:'pg',
+    connection: config
+    }
+}
+else
+{
+    module.exports = {
+    client: 'pg',
+    connection: { user: 'postgres', 
+                    database: 'sensei_koa',
+                    host: "localhost",
+                    password : 'admin1234',
+                    port : 5432}
+}
 }
